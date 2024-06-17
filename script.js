4a540cf98a65cce63a86be6bb495e20e91215234
pratma <162292827+pratmagamer@users.noreply.github.com>
2024-06-17 10:05:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+let display = document.getElementById('display');
+let currentInput = '';
+let previousInput = '';
+let operation = undefined;
+
+function appendNumber(number) {
+    if (number === '.' && currentInput.includes('.')) return;
+    currentInput = currentInput.toString() + number.toString();
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentInput === '') return;
+    if (previousInput !== '') {
+        compute();
+    }
+    operation = op;
+    previousInput = currentInput;
+    currentInput = '';
+}
+
+function compute() {
+    let computation;
+    const prev = parseFloat(previousInput);
+    const current = parseFloat(currentInput);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+        case '*':
+            computation = prev * current;
+            break;
+        case '/':
+            computation = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentInput = computation;
+    operation = undefined;
+    previousInput = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentInput = '';
+    previousInput = '';
+    operation = undefined;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.value = currentInput;
+}
+
+clearDisplay();
